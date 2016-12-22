af1249d452bb1d358f19434029c966a0c8ca8898
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-12-22 15:45:12 -0800

    Add basic Snippets list page (WIP)

--- src/components/snippets/containers/SnippetsListPage.vue
+++ src/components/snippets/containers/SnippetsListPage.vue
@@ -1,13 +1,63 @@
 <template>
   <div>
-    <h1>SnippetsListPage Component</h1>
+
+    <div class="panel panel-default" v-for="snippet in snippets">
+      <div class="panel-heading">
+        <h3 class="panel-title">{{ snippet.title }}</h3>
+      </div>
+
+      <div class="panel-body">
+        {{ snippet.url }}
+      </div>
+    </div>
+
   </div>
 </template>
 
 
 <script>
+  import {mapActions, mapGetters} from 'vuex';
+
   export default {
-    // do cool stuffs here!
+    data() {
+      return {
+        // whether any operations are currently running
+        working: false
+      };
+    },
+
+
+    computed: {
+      ...mapGetters([
+        'authToken',
+        'snippets'
+      ])
+    },
+
+
+    methods: {
+      ...mapActions([
+        'fetchSnippets'
+      ])
+    },
+
+
+    created() {
+      if (this.authToken) {
+        this.working = true;
+        this.fetchSnippets(this.authToken)
+          .then((res) => {
+            console.log('alskdjflkasjdlfksjdf');
+            this.working = false;
+          }, (err) => {
+            // TODO: show error message
+            this.working = false;
+          });
+      } else {
+        // TODO: use URL constants
+        this.$router.push('/account/login')
+      }
+    }
   };
 </script>
 
--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,2 +1,6 @@
 export const USER_LOGIN = 'USER_LOGIN';
 export const USER_LOGOUT = 'USER_LOGOUT';
+
+export const SNIPPETS = {
+  SNIPPETS_FETCH_ALL: 'SNIPPETS_FETCH_ALL'
+};
