cc5d7fe6bfc9048cf730c435a0b12c31f38bdc26
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-12-29 13:40:00 -0800

    Quick fix for checking for missing record

--- src/utils/apiHelper.js
+++ src/utils/apiHelper.js
@@ -1,9 +1,9 @@
 export default {
   findRecordById(records, id) {
     let record = records.filter(record => record.id == id);
-    if (record) {
+    if (record.length) {
       return Object.assign({}, record[0]);
     }
     return null;
   }
-}
+};
