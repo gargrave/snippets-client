328d8bccd80e8e73e0dda4e25dc8030c0769775c
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-01-12 11:20:00 -0800

    Update AccountCreatePage to use Element UI

--- src/components/account/components/AccountCreateForm.vue
+++ src/components/account/components/AccountCreateForm.vue
@@ -1,68 +1,73 @@
 <template>
-  <form @submit.prevent="onSubmit" novalidate>
+  <el-form ref="form" :model="user" :rules="rules">
 
-    <!-- email input -->
-    <app-input-field
-      inputType="text"
-      label="Email"
-      name="email"
-      placeholder="Email"
-      :value="userData.email"
-      :error="errors.email"
-      @valueChanged="onEmailChange">
-    </app-input-field>
+    <!-- email name input -->
+    <el-form-item label="Email" prop="email">
+      <span class="error-msg" v-if="apiFieldErrors.email">{{ apiFieldErrors.email }}</span>
+      <el-input
+        type="email"
+        name="email"
+        placeholder="Email"
+        v-model="user.email">
+      </el-input>
+    </el-form-item>
 
     <!-- user name input -->
-    <app-input-field
-      inputType="text"
-      label="Username"
-      name="username"
-      placeholder="Username"
-      :value="userData.username"
-      :error="errors.username"
-      @valueChanged="onUsernameChange">
-    </app-input-field>
+    <el-form-item label="Username" prop="username">
+      <span class="error-msg" v-if="apiFieldErrors.username">{{ apiFieldErrors.username }}</span>
+      <el-input
+        type="text"
+        name="username"
+        placeholder="Username"
+        v-model="user.username">
+      </el-input>
+    </el-form-item>
 
     <!-- password input -->
-    <app-input-field
-      inputType="password"
-      label="Password"
-      name="password"
-      placeholder="Password"
-      :value="userData.password"
-      :error="errors.password"
-      @valueChanged="onPasswordChange">
-    </app-input-field>
+    <el-form-item label="Password" prop="password">
+      <span class="error-msg" v-if="apiFieldErrors.password">{{ apiFieldErrors.password }}</span>
+      <el-input
+        type="password"
+        name="password"
+        placeholder="Password"
+        v-model="user.password"
+        auto-complete="off">
+      </el-input>
+    </el-form-item>
 
-    <!-- password confirm input -->
-    <app-input-field
-      inputType="password"
-      label="Confirm Password"
-      name="passwordConfirm"
-      placeholder="Confirm Password"
-      :value="userData.passwordConfirm"
-      :error="errors.passwordConfirm"
-      @valueChanged="onPasswordConfirmChange">
-    </app-input-field>
+    <!-- password input -->
+    <el-form-item label="Confirm Password" prop="passwordConfirm">
+      <span class="error-msg" v-if="apiFieldErrors.passwordConfirm">{{ apiFieldErrors.passwordConfirm }}</span>
+      <el-input
+        type="password"
+        name="passwordConfirm"
+        placeholder="Confirm Password"
+        v-model="user.passwordConfirm"
+        auto-complete="off">
+      </el-input>
+    </el-form-item>
 
 
-    <!-- submit button -->
-    <input
-      class="btn btn-primary"
-      type="submit"
-      value="Submit"
-      :disabled="working">
+    <el-form-item>
+      <!-- submit button -->
+      <el-button
+        type="primary"
+        :disabled="working"
+        @click="onSubmit">
+        Submit
+      </el-button>
 
-    <!-- cancel button -->
-    <button
-      class="btn btn-default pull-right"
-      type="button"
-      @click.prevent="onCancel"
-      :disabled="working">
-      Cancel
-    </button>
+      <!-- cancel button -->
+      <el-button
+        type="default"
+        style="float: right;"
+        :disabled="working"
+        @click="onCancel">
+        Cancel
+      </el-button>
+    </el-form-item>
 
-  </form>
+  </el-form>
 </template>
 
 
@@ -81,18 +86,8 @@
         type: Boolean,
         required: true
       },
-      // callback for form's "submit" event
-      onSubmit: {
-        type: Function,
-        required: true
-      },
-      // callback for clicking cancel button
-      onCancel: {
-        type: Function,
-        required: true
-      },
-      // any validation error messages
-      errors: {
+
+      apiFieldErrors: {
         type: Object,
         required: true
       }
@@ -100,49 +95,57 @@
 
 
     data() {
+      let passwordConfirmValidator = (rule, value, callback) => {
+        if (value !== this.user.password) {
+          callback(new Error('Passwords do not match.'));
+        } else {
+          callback();
+        }
+      };
+
       return {
         // the current data entered into the form
-        userData: {
+        user: {
           email: '',
           username: '',
           password: '',
           passwordConfirm: ''
+        },
+
+        // validation rules
+        rules: {
+          email: [
+            { required: true, message: 'Email is required.', trigger: 'blur' },
+            { type: 'email', message: 'Please enter a valid email address.', trigger: 'blur' }
+          ],
+          username: [
+            { required: true, message: 'Username is required.', trigger: 'blur' },
+            { min: 2, max: 24, message: 'Must be between 2 and 24 characters long.', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: 'Password is required.', trigger: 'blur' },
+            { min: 8, max: 50, message: 'Must be at least 8 characters long.', trigger: 'blur' }
+          ],
+          passwordConfirm: [
+            { required: true, message: 'Please re-renter your password.', trigger: 'blur' },
+            { validator: passwordConfirmValidator, trigger: 'blur,change' },
+          ]
         }
       };
     },
 
 
     methods: {
-      /**
-       * Handler for 'email' input field being edited.
-       */
-      onEmailChange(value, event) {
-        this.userData.email = value;
-        this.$emit('formDataChanged', this.userData);
+      onSubmit() {
+        this.$refs['form'].validate((valid) => {
+          if (valid) {
+            this.$emit('submitted', this.user);
+          }
+        });
       },
 
-      /**
-       * Handler for 'user name' input field being edited.
-       */
-      onUsernameChange(value, event) {
-        this.userData.username = value;
-        this.$emit('formDataChanged', this.userData);
-      },
-
-      /**
-       * Handler for 'password' input field being edited.
-       */
-      onPasswordChange(value, event) {
-        this.userData.password = value;
-        this.$emit('formDataChanged', this.userData);
-      },
-
-      /**
-       * Handler for 'confirm password' input field being edited.
-       */
-      onPasswordConfirmChange(value, event) {
-        this.userData.passwordConfirm = value;
-        this.$emit('formDataChanged', this.userData);
+      onCancel() {
+        this.$emit('cancelled');
       }
     }
   };
--- src/components/account/containers/AccountCreatePage.vue
+++ src/components/account/containers/AccountCreatePage.vue
@@ -1,43 +1,47 @@
 <template>
-  <div>
+  <section>
     <h2 class="page-title">New Account</h2>
-    <!-- 'working' icon -->
-    <app-loading-icon v-if="working"></app-loading-icon>
 
-    <div class="panel panel-primary snippet-panel">
+    <el-card
+      class="box-card"
+      v-loading="working"
+      element-loading-text="Creating account..."
+      style="width: 100%">
 
-      <div class="panel-heading">
-        <h3 class="panel-title">User Details</h3>
-      </div><!-- /panel-heading -->
+      <div class="text item">
 
-      <div class="panel-body">
+        <!-- error message display -->
+        <el-alert
+          title="Account Creation Error"
+          type="error"
+          v-if="apiError"
+          :description="apiError"
+          :closable="false">
+        </el-alert>
 
+        <!-- 'new account' form -->
         <app-account-create-form
           :working="working"
-          :onSubmit="onSubmit"
-          :onCancel="onCancel"
-          :errors="errors"
-          @formDataChanged="onFormChanged">
+          :apiFieldErrors="apiFieldErrors"
+          @submitted="onFormSubmitted"
+          @cancelled="onFormCancelled">
         </app-account-create-form>
 
-      </div><!-- /panel-body -->
-    </div><!-- /panel -->
-  </div>
+      </div><!-- /.text item -->
+    </el-card>
+
+  </section>
 </template>
 
 
 <script>
   import { mapActions } from 'vuex';
-  import toastr from 'toastr';
 
   import { localUrls } from '../../../appData/urls';
-  import validate from '../../../utils/validate';
-  import LoadingIcon from '../../common/components/LoadingIcon';
   import AccountCreateForm from '../components/AccountCreateForm.vue';
 
   export default {
     components: {
-      appLoadingIcon: LoadingIcon,
       appAccountCreateForm: AccountCreateForm
     },
 
@@ -50,16 +54,9 @@
         // error messages returned from API (e.g. invalid login)
         apiError: '',
 
-        // local validation errors (e.g. missing field)
-        errors: {
-          email: '',
-          username: '',
-          password: '',
-          passwordConfirm: ''
-        },
-
-        // model for user login data
-        userData: {
+        // any field-specific error message returned from the API
+        // e.g. "password is too common"
+        apiFieldErrors: {
           email: '',
           username: '',
           password: '',
@@ -71,114 +68,54 @@
 
     methods: {
       /**
-       * Handler for input fields being changed on login form.
+       * Attempts to submit the current user data to the API to login.
        */
-      onFormChanged(value) {
-        const previousUsername = this.userData.username;
-
-        this.userData = {
+      onFormSubmitted(value, event) {
+        const user = {
           email: value.email,
           username: value.username,
           password: value.password,
           passwordConfirm: value.passwordConfirm
         };
 
-        if (previousUsername !== value.username) {
-          // TODO: check if username exists
-        }
-      },
-
-      /**
-       * Attempts to submit the current user data to the API to login.
-       * Note that if it does not validate, no request is sent.
-       */
-      onSubmit() {
-        if (this.isValid()) {
-          this.working = true;
-          this.errors = {
-            email: '',
-            username: '',
-            password: '',
-            passwordConfirm: ''
-          };
-
-          this.createUser(this.userData)
-            .then(() => {
-              this.$router.push(localUrls.account);
-              toastr.success('Account created');
-              this.working = false;
-            }, (err) => {
-              this.apiError = 'Unable to create account';
-              // show any errors returned from the API
-              // these will be errors that we cannot validate on the front-end,
-              // like 'username already in use' or 'password is too simple'
-              this.errors = {
-                email: err.email ? err.email[0] : '',
-                username: err.username ? err.username[0] : '',
-                password: err.password1 ? err.password1[0] : '',
-                passwordConfirm: err.password2 ? err.password2[0] : ''
-              };
-              this.working = false;
-            });
-        }
-      },
-
-      /**
-       * Handler for form's 'cancel' button; redirect to login page.
-       */
-      onCancel() {
-        this.$router.push(localUrls.login);
-      },
-
-      /**
-       * Validates the user data currently entered into the form.
-       *
-       * @returns {boolean} Whether the data validates correctly.
-       */
-      isValid() {
-        let valid = true;
-        let params;
-
-        this.errors = {
+        // clear any existing API errors
+        this.apiFieldErrors = {
           email: '',
           username: '',
           password: '',
           passwordConfirm: ''
         };
 
-        // validate email
-        params = { required: true, format: 'email' };
-        const emailVal = validate(this.userData.email, params);
-        if (!emailVal.valid) {
-          this.errors.email = emailVal.error;
-          valid = false;
-        }
+        this.working = true;
+        this.createUser(user)
+          .then(() => {
+            // if successful, redirect to the 'account' page
+            this.$router.push(localUrls.account);
+            this.$notify({
+              message: 'Account created',
+              type: 'success'
+            });
+            this.working = false;
+          }, (err) => {
+            this.apiError = 'Unable to create account';
+            // show any errors returned from the API
+            // these will be errors that we cannot validate on the front-end,
+            // like 'username already in use' or 'password is too simple'
+            this.apiFieldErrors = {
+              email: err.email ? err.email[0] : '',
+              username: err.username ? err.username[0] : '',
+              password: err.password1 ? err.password1[0] : '',
+              passwordConfirm: err.password2 ? err.password2[0] : ''
+            };
+            this.working = false;
+          });
+      },
 
-        // validate username
-        params = { required: true, minLength: 2 };
-        const userVal = validate(this.userData.username, params);
-        if (!userVal.valid) {
-          this.errors.username = userVal.error;
-          valid = false;
-        }
-
-        // validate password
-        params = { required: true, minLength: 8 };
-        const passVal = validate(this.userData.password, params);
-        if (!passVal.valid) {
-          this.errors.password = passVal.error;
-          valid = false;
-        }
-
-        // validate confirmation password (only check that they match)
-        const p1 = this.userData.password;
-        const p2 = this.userData.passwordConfirm;
-        if (p1 !== p2 || p2 === '') {
-          this.errors.passwordConfirm = 'Passwords do not match.';
-          valid = false;
-        }
-
-        return valid;
+      /**
+       * Handler for form's 'cancel' button; redirect to login page.
+       */
+      onFormCancelled() {
+        this.$router.push(localUrls.login);
       },
 
       ...mapActions([
