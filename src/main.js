553c6a79550d11c35599e8d0814108f4c47a5b79
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-01-17 13:47:11 -0800

    Remove toastr

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import {store} from './store/store';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import '../node_modules/toastr/build/toastr.min.css';
 import '../static/main.scss';
 
 Vue.use(VueRouter);
